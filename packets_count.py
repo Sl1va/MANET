807a0384299fb59471849341b0f432e859e8d972
Hephzibah8625 <azamat02203@gmail.com>
2024-05-31 15:07:27 +0300

    Add int results in normalization

--- packets_count.py
+++ packets_count.py
@@ -14,13 +14,13 @@ file_prefixes = [home_dir + i for i in file_prefixes]
 def sum_by_seconds(var1, var2):
     for t in range(0, 101):
         if str(t) not in var2: continue
-        var1[str(t)] += (var2[str(t)] * 1.0 / 10)
+        var1[str(t)] += var2[str(t)]
 
 
 def sum_by_nodes(var1, var2):
     for n in range(0, 25):
         if str(n) not in var2: continue
-        var1[str(n)] += (var2[str(n)] * 1.0 / 10)
+        var1[str(n)] += var2[str(n)]
 
 
 def sum_by_seconds_by_nodes(var1, var2):
@@ -28,18 +28,18 @@ def sum_by_seconds_by_nodes(var1, var2):
         if str(t) not in var2: continue
         for n in range(0, 25):
             if str(n) not in var2[str(t)]: continue
-            var1[str(t)][str(n)] += (var2[str(t)][str(n)] * 1.0 / 10)
+            var1[str(t)][str(n)] += var2[str(t)][str(n)]
 
 
 for file_prefix in file_prefixes:
     total_packets_sent = 0
     total_packets_received = 0
-    packets_sent_by_node = defaultdict(float)
-    packets_received_by_node = defaultdict(float)
-    packets_sent_per_second = defaultdict(float)
-    packets_received_per_second = defaultdict(float)
-    packets_sent_per_second_by_node = defaultdict(lambda: defaultdict(float))
-    packets_received_per_second_by_node = defaultdict(lambda: defaultdict(float))
+    packets_sent_by_node = defaultdict(int)
+    packets_received_by_node = defaultdict(int)
+    packets_sent_per_second = defaultdict(int)
+    packets_received_per_second = defaultdict(int)
+    packets_sent_per_second_by_node = defaultdict(lambda: defaultdict(int))
+    packets_received_per_second_by_node = defaultdict(lambda: defaultdict(int))
 
     save_filename = file_prefix + "norm.json"
 
@@ -61,7 +61,23 @@ for file_prefix in file_prefixes:
             sum_by_seconds_by_nodes(packets_sent_per_second_by_node, data["packets_sent_per_second_by_node"])
             sum_by_seconds_by_nodes(packets_received_per_second_by_node, data["packets_received_per_second_by_node"])
 
+    total_packets_sent /= 10
+    total_packets_received /= 10
+
+    for n in range(0, 25):
+        packets_sent_by_node[str(n)] /= 10
+        packets_received_by_node[str(n)] /= 10
     
+    for t in range(0, 101):
+        packets_sent_per_second[str(t)] /= 10
+        packets_received_per_second[str(t)] /= 10
+    
+    for t in range(0, 101):
+        for n in range(0, 25):
+            packets_sent_per_second_by_node[str(t)][str(n)] /= 10
+            packets_received_per_second_by_node[str(t)][str(n)] /= 10
+
+
     # Generate the report
     report = {
         'total_packets_sent': total_packets_sent,
